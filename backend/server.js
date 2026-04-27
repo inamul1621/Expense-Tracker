2c6b2c7eb91207ef55eda42b3b7ece7acd9023b0
Inamul Islam <islaminamul1604@gmail.com>
2026-04-27 22:21:20 +0530

    Update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://expense-tracker-chi-ten-39.vercel.app',
+  'expense-tracker-lyart-ten-46.vercel.app',
 ];
 
 // Allow all origins in development, restrict in production
